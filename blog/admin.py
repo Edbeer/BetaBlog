ce0e2ebfa37ce33cbf170926d93e3c10b283222c
Edbeer <edbeermtn@gmail.com>
2021-08-26 00:20:53 +0700

    add ckeditor

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django import forms
 from .models import *
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
 
 admin.site.register(Post)
 admin.site.register(Category)
--- myblog/urls.py
+++ myblog/urls.py
@@ -21,8 +21,9 @@ from django.urls import path, include
 import debug_toolbar
 
 urlpatterns = [
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
